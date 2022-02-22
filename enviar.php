2d1ef73eebb53f0cbdbae5c3273760007f40ddc1
Hualdo <hucastillo@gmail.com>
2022-02-21 19:18:46 -0500

    trying form button

--- enviar.php
+++ enviar.php
@@ -9,15 +9,15 @@ $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
-$messaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$messaje .= "Su e-mail es: " . $mail . " \r\n";
-$messaje .= "Mensaje: " . $_POST['messaje'] . " \r\n";
-$messaje .= "Enviado el " . date('d/m/Y', time());
+$message = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Mensaje: " . $_POST['messaje'] . " \r\n";
+$message .= "Enviado el " . date('d/m/Y', time());
 
 $para = 'hucastillo@gmail.com';
 $asunto = 'Mensaje de mi sitio web';
 
-mail($para, $subject, utf8_decode($messaje), $header);
+mail($para, $subject, utf8_decode($message), $header);
 
 header("Location:index.html");
 ?>
